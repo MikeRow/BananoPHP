784513b243a787285bb7592130d42639189dcf3d
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-03-03 16:46:33 +0100

    Update ncm.php

--- ncm/ncm.php
+++ ncm/ncm.php
@@ -2247,47 +2247,6 @@
 		To lose it all
 		But in the end
 		It doesn't even matter
-		One thing, I don't know why
-		It doesn't even matter how hard you try
-		Keep that in mind
-		I designed this rhyme
-		To remind myself of a time when
-		I tried so hard
-		In spite of the way you were mocking me
-		Acting like I was part of your property
-		Remembering all the times you fought with me
-		I'm surprised it got so
-		Things aren't the way they were before
-		You wouldn't even recognize me anymore
-		Not that you knew me back then
-		But it all comes back to me in the end
-		You kept everything inside
-		And even though I tried, it all fell apart
-		What it meant to me will eventually be a memory of a time when
-		I tried so hard
-		And got so far
-		But in the end
-		It doesn't even matter
-		I had to fall
-		To lose it all
-		But in the end
-		It doesn't even matter
-		I've put my trust in you
-		Pushed as far as I can go
-		For all this
-		There's only one thing you should know
-		I've put my trust in you
-		Pushed as far as I can go
-		For all this
-		There's only one thing you should know
-		I tried so hard
-		And got so far
-		But in the end
-		It doesn't even matter
-		I had to fall
-		To lose it all
-		But in the end
-		It doesn't even matter
 	
 	*/
 	
