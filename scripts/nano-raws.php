50edaa1b958de2ebd57840b162f90b96e36864b5
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-04-26 23:11:20 +0200

    Update

--- scripts/nano-raws.php
+++ scripts/nano-raws.php
@@ -1,7 +1,6 @@
 <?php
 
 	/*
-	
 		Nano uses huge integers to represent even a tiny value, for example:
 		
 			1 NANO = 1 Mnano = 1000000000000000000000000000000000000 raw
@@ -9,16 +8,11 @@
 		Since PHP doesn't support mathematical operations with such huge integers, you need a dedicated library
 		
 		GNU Multiple Precision (GMP) fits this work: https://www.php.net/manual/en/book.gmp.php
-	
 	*/
 
 	$raw1 = '2000000000000000000000000000000000000'; // 2 NANO
-	
 	$raw2 = '1000000000000000000000000000000000000'; // 1 NANO
-	
 	$raw3 = '2000000000000000000000000000000000000'; // 2 NANO
-	
-	
 	
 	// Sum of raws
 	
@@ -39,9 +33,7 @@
 	// Comparison of raws
 	
 	echo gmp_cmp( $raw1, $raw2 ) . PHP_EOL; // Prints 1
-	
 	echo gmp_cmp( $raw2, $raw1 ) . PHP_EOL; // Prints -1
-	
 	echo gmp_cmp( $raw1, $raw3 ) . PHP_EOL; // Prints 0
 
 ?>
--- scripts/ncm-remote.php
+++ scripts/ncm-remote.php
@@ -1,65 +1,49 @@
 <?php
 
 	/*
-	
 		Manage a node via SSH using a script and ncm
 		
 		This script use a custom function "ncmCall" to simplify ncm call procedure
 		Some flags like json_in,json_out,no_confirm are set as default due to the nature of the custom function
-
 	*/
-
 
 
 	// *** Includes ***
 
 
-
 	require_once __DIR__ . '/../lib3/phpseclib_loader.php';
 
 	use phpseclib\Crypt\RSA;
-	
 	use phpseclib\Net\SSH2;
 	
 	
-	
 	// *** ncm call ***
-	
 	
 	
 	$ncm_path = '/home/nano/php4nano/ncm/ncm.php';
 	
 	function ncmCall( &$ssh, string $ncm_path, string $command, array $arguments, string $flags = '', string $callerID = 'remote-script' )
 	{
-		
 		if( $flags != '' ) $flags .= ',';
 		
 		$flags .= 'json_in,json_out,no_confirm';
-		
 		$return = $ssh->exec( "php $ncm_path $command '" . json_encode( $arguments ) . "' flags=$flags callerID=$callerID" . PHP_EOL );
 		
 		return json_decode( $return, true );
-		
 	}
-	
 	
 	
 	// *** SSH connection ***
 
 
-
 	$hostname = 'localhost';
-	
 	$username = 'nano';
-	
 	$privkeyfile_path = 'path/to/private/key/file';
 
 	//
 
 	$ssh = new SSH2( $hostname );
-	
 	$key = new RSA();
-	
 	$key->loadKey( file_get_contents( $privkeyfile_path ) );
 	
 	if( !$ssh->login( $username, $key ) )
@@ -68,9 +52,7 @@
 	}
 
 
-
 	// *** Execution ***
-	
 	
 	
 	// Call 1
@@ -91,9 +73,7 @@
 	print_r( $ncmCall );
 
 
-
 	// *** Disconnection ***
-	
 	
 	
 	$ssh->disconnect();
