490c0b35f66050b319c3385418a3f5b29591c579
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-05-05 18:26:30 +0200

    Update nco.php

--- nco/nco.php
+++ nco/nco.php
@@ -459,11 +459,14 @@
 					{
 						foreach( $ncmCall as $wallet_id => $wallet_info )
 						{
-							$wallets_count++;
-							$wallets_balance = gmp_add( $wallets_balance, $wallet_info['balance'] );
-							$wallets_pending = gmp_add( $wallets_pending, $wallet_info['pending'] );
-							$wallets_weight = gmp_add( $wallets_weight, $wallet_info['weight'] );
-							$wallets_accounts_count += $wallet_info['accounts_count'];
+							if( is_array( $wallet_info ) )
+							{
+								$wallets_count++;
+								$wallets_balance = gmp_add( $wallets_balance, $wallet_info['balance'] );
+								$wallets_pending = gmp_add( $wallets_pending, $wallet_info['pending'] );
+								$wallets_weight = gmp_add( $wallets_weight, $wallet_info['weight'] );
+								$wallets_accounts_count += $wallet_info['accounts_count'];
+							}
 						}
 					}
 					
