d1dbd9c4103f059ed212964197ebcdc0f02ff656
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-05-11 11:08:36 +0200

    Update NanoRPCExtension.php

--- lib/NanoRPCExtension.php
+++ lib/NanoRPCExtension.php
@@ -34,7 +34,7 @@
 			
 			$wallet_info = $this->wallet_info( ['wallet' => $wallet] );
 			
-			if( isset( $wallet_info['error'] ) )
+			if( $wallet_info == false )
 			{
 				return ['error'=>'Bad wallet number'];
 			}
@@ -157,7 +157,7 @@
 			
 			$wallet_info = $this->wallet_info( ['wallet' => $wallet] );
 			
-			if( isset( $wallet_info['error'] ) )
+			if( $wallet_info == false )
 			{
 				return ['error'=>'Bad wallet number'];
 			}
@@ -312,7 +312,7 @@
 			
 			$wallet_info = $this->wallet_info( ['wallet' => $wallet] );
 			
-			if( isset( $wallet_info['error'] ) )
+			if( $wallet_info == false )
 			{
 				return ['error'=>'Bad wallet number'];
 			}
