db52ac91723d7a7cc0720bb1ee78140b4a8835ae
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-05-14 00:05:33 +0200

    Update

--- lib/NanoTools.php
+++ lib/NanoTools.php
@@ -185,9 +185,6 @@
 		
 		
 		
-		// *** Using Salt ***
-		
-		
 		public static function account2public( string $account )
 		{
 			if( ( strpos( $account, 'xrb_1' ) === 0 || strpos( $account, 'xrb_3' ) === 0 || strpos( $account, 'nano_1' ) === 0 || strpos( $account, 'nano_3' ) === 0 ) && ( strlen( $account ) == 64 || strlen( $account ) == 65 ) )
@@ -203,48 +200,25 @@
 					$hash_uint8 = Uint::fromString( substr( $crop, 52, 60 ) )->toUint8()->toArray();
 					$key_uint8 = Uint::fromUint4Array( $key_uint4 )->toUint8();
 					
-					$key_hash = new SplFixedArray( 64 );
-					
-					$b2b = new Blake2b();
-					$ctx = $b2b->init( null, 5 );
-					$b2b->update( $ctx, $key_uint8, count( $key_uint8 ) );
-					$b2b->finish( $ctx, $key_hash );
-					
-					$key_hash = array_reverse( array_slice( $key_hash->toArray(), 0, 5 ) );
-					
-					if( $hash_uint8 == $key_hash )
+					if( !extension_loaded( 'blake2' ) )
 					{
-						return Uint::fromUint4Array( $key_uint4 )->toHexString();
+						$key_hash = new SplFixedArray( 64 );
+						
+						$b2b = new Blake2b();
+						$ctx = $b2b->init( null, 5 );
+						$b2b->update( $ctx, $key_uint8, count( $key_uint8 ) );
+						$b2b->finish( $ctx, $key_hash );
+						
+						$key_hash = array_reverse( array_slice( $key_hash->toArray(), 0, 5 ) );
 					}
-				}
-			}
-			
-			return false;
-		}
-		
-		
-		// *** Using php-blake2 ***
-		
-		
-		public static function account2public_ext( string $account )
-		{
-			if( ( strpos( $account, 'xrb_1' ) === 0 || strpos( $account, 'xrb_3' ) === 0 || strpos( $account, 'nano_1' ) === 0 || strpos( $account, 'nano_3' ) === 0 ) && ( strlen( $account ) == 64 || strlen( $account ) == 65 ) )
-			{
-				$crop = explode( '_', $account );
-				$crop = $crop[1];
-				
-				if( preg_match( '/^[13456789abcdefghijkmnopqrstuwxyz]+$/', $crop ) )
-				{
-					$aux = Uint::fromString( substr( $crop, 0, 52 ) )->toUint4()->toArray();
-					array_shift( $aux );
-					$key_uint4 = $aux;
-					$hash_uint8 = Uint::fromString( substr( $crop, 52, 60 ) )->toUint8()->toArray();
-					$key_uint8 = Uint::fromUint4Array( $key_uint4 )->toUint8();
-					$key_uint8 = self::bin_arr2str( (array) $key_uint8 );
-					
-					$key_hash = blake2( $key_uint8, 5, null, true );
-					$key_hash = self::bin_str2arr( $key_hash );
-					$key_hash = array_reverse( array_slice( $key_hash, 0, 5 ) );
+					else
+					{
+						$key_uint8 = self::bin_arr2str( (array) $key_uint8 );
+						
+						$key_hash = blake2( $key_uint8, 5, null, true );
+						$key_hash = self::bin_str2arr( $key_hash );
+						$key_hash = array_reverse( array_slice( $key_hash, 0, 5 ) );
+					}
 					
 					if( $hash_uint8 == $key_hash )
 					{
@@ -264,44 +238,36 @@
 		
 		
 		
-		// *** Using Salt ***
-		
-		
 		public static function public2account( string $pk )
 		{
 			if( strlen( $pk ) != 64 || !hex2bin( $pk ) ) return false;
+
+			if( !extension_loaded( 'blake2' ) )
+			{
+				$key = Uint::fromHex( $pk );
+				$checksum;
+				$hash = new SplFixedArray( 64 );
+				
+				$b2b = new Blake2b();
+				$ctx = $b2b->init( null, 5 );
+				$b2b->update( $ctx, $key->toUint8(), 32 );
+				$b2b->finish( $ctx, $hash );
+				
+				$hash = Uint::fromUint8Array( array_slice( $hash->toArray(), 0, 5 ) )->reverse();
+				$checksum = $hash->toString();
+			}
+			else
+			{
+				$key = Uint::fromHex( $pk )->toUint8();
+				$key = self::bin_arr2str( (array) $key );
+				
+				$hash = blake2( $key, 5, null, true );
+				$hash = self::bin_str2arr( $hash );
+				$hash = array_reverse( array_slice( $hash, 0, 5 ) );
+				
+				$checksum = Uint::fromUint8Array( $hash )->toString();
+			}
 			
-			$key = Uint::fromHex( $pk );
-			$checksum;
-			$hash = new SplFixedArray( 64 );
-			$b2b = new Blake2b();
-			$ctx = $b2b->init( null, 5 );
-			$b2b->update( $ctx, $key->toUint8(), 32 );
-			$b2b->finish( $ctx, $hash );
-			$hash = Uint::fromUint8Array( array_slice( $hash->toArray(), 0, 5 ) )->reverse();
-			
-			$checksum = $hash->toString();
-			$c_account = Uint::fromHex( '0' . $pk )->toString();
-			
-			return 'nano_' . $c_account . $checksum;
-		}
-		
-		
-		// *** Using php-blake2 ***
-		
-		
-		public static function public2account_ext( string $pk )
-		{
-			if( strlen( $pk ) != 64 || !hex2bin( $pk ) ) return false;
-			
-			$key = Uint::fromHex( $pk )->toUint8();
-			$key = self::bin_arr2str( (array) $key );
-			
-			$hash = blake2( $key, 5, null, true );
-			$hash = self::bin_str2arr( $hash );
-			$hash = array_reverse( array_slice( $hash, 0, 5 ) );
-			
-			$checksum = Uint::fromUint8Array( $hash )->toString();
 			$c_account = Uint::fromHex( '0' . $pk )->toString();
 			
 			return 'nano_' . $c_account . $checksum;
@@ -313,9 +279,6 @@
 		// *** Private key to public key ***
 		// *********************************
 		
-		
-		
-		// *** Using Salt ***
 		
 		
 		public static function private2public( string $sk )
@@ -338,9 +301,6 @@
 		
 		
 		
-		// *** Using Salt ***
-		
-		
 		public static function keys( bool $get_account = false )
 		{
 			$salt = Salt::instance();
@@ -359,9 +319,6 @@
 		// *** Get keys from seed ***
 		// **************************
 		
-		
-		
-		// *** Using Salt ***
 		
 		
 		public static function seed2keys( string $seed, int $index = 0, bool $get_account = false )
@@ -406,9 +363,6 @@
 		
 		
 		
-		// *** Using Salt ***
-		
-		
 		public static function hash_array( array $inputs, int $size = 64 )
 		{
 			if( $size <1 ) return false;
@@ -441,9 +395,6 @@
 		
 		
 		
-		// *** Using Salt ***
-		
-		
 		public static function sign( $sk, $msg )
 		{
 			if( strlen( $sk ) != 64 || !hex2bin( $sk ) ) return false;
@@ -469,9 +420,6 @@
 		// *** Validate a signature ***
 		// ****************************
 		
-		
-		
-		// *** Using Salt ***
 		
 		
 		public static function sign_validate( $msg, $sig, $account )
@@ -500,55 +448,10 @@
 		// ***********************
 		
 			
-			
-		// *** Using Salt ***
-			
 		/*
 		public static function work( string $hash, string $difficulty )
 		{
-			if( strlen( $hash ) != 64 || !hex2bin( $hash ) ) return false;
-			if( strlen( $difficulty ) != 16 || !hex2bin( $difficulty ) ) return false;
-			
-			$b2b = new Blake2b();
-			
-			$hash = Uint::fromHex( $hash )->toUint8();
-			$difficulty = hexToDec( $difficulty );
-			$work = new SplFixedArray( 64 );
-			
-			$o = 1;
-			while( true )
-			{
-				$rng = [];
-				for ($i = 0; $i < 8; $i++) $rng[] = mt_rand( 0, 255 );
-				$rng = Uint::fromUint8Array( $rng )->toUint8();
-				$work = new SplFixedArray( 64 );
-				
-				$ctx = $b2b->init( null, 8 );
-				$b2b->update( $ctx, $rng, 8 );
-				$b2b->update( $ctx, $hash, 32 );
-				$b2b->finish( $ctx, $work );
-				
-				$work = $work->toArray();
-				$work = array_slice( $work, 0, 8 );
-				$work = array_reverse( $work );
-				$work = Uint::fromUint8Array( $work )->toHexString();
-				//echo $work; exit;
-				//$work = array_slice( $work->toArray(), 0, 8 );
-				//$work = Uint::fromUint8Array( $work )->toHexString();
-				//$work = array_slice( Uint::fromUint8Array($work)->toArray(), 0, 8 )->toHexString();
-				echo $o . PHP_EOL;
-				$o++;
-				//echo hexToDec( $work ) . '-' . $difficulty. PHP_EOL;
-				if( hexToDec( $work ) >= $difficulty ) return $work;
-			}
-		}
-		*/
-		
-		// *** Using php-blake2 ***
-		
-		/*
-		public static function work_ext( string $hash, string $difficulty )
-		{
+			if( !extension_loaded( 'blake2' ) ) return false,
 			if( strlen( $hash ) != 64 || !hex2bin( $hash ) ) return false;
 			if( strlen( $difficulty ) != 16 || !hex2bin( $difficulty ) ) return false;
 			
@@ -588,9 +491,6 @@
 		// *** Validate work ***
 		// *********************
 		
-		
-		
-		// *** Using Salt ***
 		
 		
 		public static function work_validate( string $hash, string $work, string $difficulty )
