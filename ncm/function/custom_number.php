21a135286b9f6aede0f415655cab00f9af290585
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-05-19 22:30:33 +0200

    Update custom_number.php

--- ncm/function/custom_number.php
+++ ncm/function/custom_number.php
@@ -7,34 +7,7 @@
 	{
 		// $number = sprintf( "%s", $number );
 		
-		if( $decimals < 0 )
-		{
-			$amount_array = explode( '.', $number );
-			
-			if( isset( $amount_array[1] ) )
-			{
-				// Remove useless decimals
-				
-				while( substr( $amount_array[1], -1 ) == '0' )
-				{
-					$amount_array[1] = substr( $amount_array[1], 0, -1 );
-				}
-				
-				if( strlen( $amount_array[1] ) < 1 )
-				{
-					return number_format( $amount_array[0], 0, '', $thousand );
-				}
-				else
-				{
-					return number_format( $amount_array[0], 0, '', $thousand ) . '.' . $amount_array[1];
-				}
-			}
-			else
-			{
-				return number_format( floor( $number ), 0, '', $thousand );
-			}
-		}
-		elseif( $decimals == 0 )
+		if( $decimals == 0 )
 		{
 			return number_format( floor( $number ), 0, $decimal, $thousand );
 		}
@@ -57,7 +30,14 @@
 				}
 				else
 				{
-					return number_format( $amount_array[0], 0, '', $thousand ) . '.' . substr( $amount_array[1], 0, $decimals );
+					if( $decimals < 0 )
+					{
+						return number_format( $amount_array[0], 0, '', $thousand ) . '.' . $amount_array[1];
+					}
+					else 
+					{
+						return number_format( $amount_array[0], 0, '', $thousand ) . '.' . substr( $amount_array[1], 0, $decimals );
+					}
 				}
 			}
 			else
