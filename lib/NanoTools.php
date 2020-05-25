541948b93bab60fc482cc7cb5bb9549022af46ec
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-05-25 11:07:53 +0200

    Update

--- lib/NanoTools.php
+++ lib/NanoTools.php
@@ -473,10 +473,9 @@
 		
 		
 		
-		public static function message( array $inputs, int $size = 64 )
+		public static function message( array $inputs )
 		{
 			if( count( $inputs ) != 6 ) return false;
-			if( $size <1 ) return false;
 			
 			$b2b = new Blake2b();
 			
