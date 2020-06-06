17f583f900d2dee0ae1c9c7dd92923894bb9ab55
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-06-06 16:50:20 +0200

    Update

--- tests/NanoTools/dec2hex.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../lib/NanoTools.php';
-
-use php4nano\NanoTools as NanoTools;
-
-var_dump(NanoTools::dec2hex('115522015506741413552468376787470218603871886200306080305044391135369953419741'));
--- tests/NanoTools/hex2dec.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../lib/NanoTools.php';
-
-use php4nano\NanoTools as NanoTools;
-
-var_dump(NanoTools::hex2dec('ff6724c892fa21ddff6724c892fa21ddff6724c892fa21ddff6724c892fa21dd'));
