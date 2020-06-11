f86cfb3878c355d33efcd48edb90a4600ad6bfdf
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-06-11 08:11:53 +0200

    Update

--- test/NanoTool/account2public.php
+++ test/NanoTool/account2public.php
@@ -4,6 +4,14 @@ require_once __DIR__ . '/../../autoload.php';
 
 use php4nano\NanoTool as NanoTool;
 
+$t0 = microtime(true);
+
 var_dump(NanoTool::account2public('nano_3ieu9rjq8uyd3h1taykfb3s14g5p6mnu73hep4iox8w91hew3147eejgogxx'));
 
+$t1 = microtime(true);
+echo ($t1 - $t0) . PHP_EOL;
+
 var_dump(NanoTool::account2public('nano_3ieu9rjq8uyd3h1taykfb3s14g5p6mnu73hep4iox8w91hew3147eejgogxx', false));
+
+$t2 = microtime(true);
+echo ($t2 - $t1) . PHP_EOL;
--- test/NanoTool/signMsg.php
+++ test/NanoTool/signMsg.php
@@ -7,4 +7,9 @@ use php4nano\NanoTool as NanoTool;
 $private_key = '0F83D2E2B768F59238783FCEA893B39105D6E0E944523B3E6B73757D7A29970C';
 $msg         = '36E778DEDF4094AD9424C28F3198150328FD33B9A08BEA88C177A11B898E156B';
 
+$t0 = microtime(true);
+
 var_dump(NanoTool::signMsg($private_key, $msg));
+
+$t1 = microtime(true);
+echo ($t1 - $t0) . PHP_EOL;
