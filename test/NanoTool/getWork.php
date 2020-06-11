657ff7961d92e46e67855bf095fb61f65473693c
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-06-11 09:36:02 +0200

    Update getWork.php

--- test/NanoTool/getWork.php
+++ test/NanoTool/getWork.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../../autoload.php';
 use php4nano\NanoTool as NanoTool;
 
 $hash       = 'A36B0B8CC84253E57C90E959755816EA51F00FA3497B8D2C665551FAECFBD0D0';
-$difficulty = 'ff00000000000000';
+$difficulty = 'ffffffc000000000';
 
 var_dump(NanoTool::getWork($hash, $difficulty));
