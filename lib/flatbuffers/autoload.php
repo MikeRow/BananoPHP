701f63892f1d99cd48ffb224e332a078e5035684
Mike Row <47996463+mikerow@users.noreply.github.com>
2020-07-12 13:44:19 +0200

    Fixed autoload

--- lib/flatbuffers/autoload.php
+++ lib/flatbuffers/autoload.php
@@ -1,16 +1,8 @@
 <?php 
 
-spl_autoload_register(
-    function($class) {
-        $class = substr($class, strrpos($class, "\\") + 1);
-        $root_dir = join(DIRECTORY_SEPARATOR, array(dirname(dirname(__FILE__)))); // `flatbuffers` root.
-        $paths = array(join(DIRECTORY_SEPARATOR, array($root_dir, "flatbuffers/nanoapi")));
-        foreach ($paths as $path) {
-            $file = join(DIRECTORY_SEPARATOR, array($path, $class . ".php"));
-            if (file_exists($file)) {
-                require $file;
-                break;
-            }
-        }
+$files = scandir(__DIR__ . '/nanoapi');
+foreach ($files as $file) {
+    if ($file != '.' && $file != '..') {
+        require __DIR__ . '/nanoapi/' . $file;
     }
-);
+}
